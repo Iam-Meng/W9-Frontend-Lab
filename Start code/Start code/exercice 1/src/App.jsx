adca864143a39ee542a64be564791e182b25ebc1
Iam-Meng <pochseavmeng@gmail.com>
2026-03-15 15:27:41 +0700

    merge-solution

--- Start code/Start code/exercice 1/src/App.jsx
+++ Start code/Start code/exercice 1/src/App.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 
 export default function App() {
-<<<<<<< HEAD
   // State to store the score value entered by the user
-  const [score, setScore] = React.useState(4);
+  const [score, setScore] = React.useState(0);
 
   const getScoreBarStyle = () => {
     // 1- Compute width
@@ -19,24 +18,6 @@ export default function App() {
       scoreColor = "#9cc54b94";
     } else {
       scoreColor = "#0af616";
-=======
-  const [score, setScore] = React.useState(0);
-
-  const getScoreBarStyle = () => {
-    // 1- Compute width based on current score (0–10)
-    const scoreWidth = `${(score / 10) * 100}%`;
-
-    // 2- Compute color (optional)
-    let scoreColor = `#f3bc47`;
-    if (score >= 9) {
-      scoreColor = "#1cde23";
-    } else if (score >= 7) {
-      scoreColor = "#3caf4099";
-    } else if (score <= 4) {
-      scoreColor = "#f44336";
-    } else {
-      scoreColor = "#f3bc47";
->>>>>>> 3862f8562ab0d87d10de12bdf73f16703d8d274b
     }
 
     // 3 - Return the style object
@@ -54,17 +35,10 @@ export default function App() {
         <small>Enter a score (0 to 10): </small>
         <input type="number" min="0" max="10" value={score}
           onChange={(e) => {
-<<<<<<< HEAD
             const val = Math.min(10, Math.max(0, Number(e.target.value)));
             setScore(val);
           }}
         ></input>
-=======
-            const v = parseFloat(e.target.value);
-            if (!isNaN(v)) setScore(Math.min(Math.max(v, 0), 10));
-          }}
-        />
->>>>>>> 3862f8562ab0d87d10de12bdf73f16703d8d274b
 
         <div className="score-bar">
           <div className="score-bar-value" style={getScoreBarStyle()}></div>
